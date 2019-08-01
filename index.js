6ab3e2bb2b6c21b87cd2ad06b48abb7b203a941a
lunaroyster <oneinaquadrillion@live.com>
2019-08-01 15:43:29 +0530

    Content

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = new express();
+
+app.use('/useragent', async (req, res) => {
+  let useragent = req.headers['user-agent'];
+  console.log(useragent);
+  res.send(`
+  <html>
+    <head>
+      <meta property="og:title" content="Your user agent is: ${useragent}">
+      <meta property="og:description" content="Generated at ${new Date().toString()}">
+      <meta name="twitter:card" content="summary_large_image">
+    </head>
+    <body>
+      ${useragent}
+    </body>
+  </html>
+  `);
+})
+
+app.listen(8080, 'localhost');
